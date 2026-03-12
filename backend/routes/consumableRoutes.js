958d7b43ec3c633616624f394474978dcf765d56
Neeraj <neerajarvind582@gmail.com>
2026-03-12 18:02:30 +0530

    update

--- backend/routes/consumableRoutes.js
+++ backend/routes/consumableRoutes.js
@@ -1,37 +1,3 @@
-// import express from "express";
-// const router = express.Router();
-// import {
-//   getConsumables,
-//   getConsumableById,
-//   createConsumable,
-//   assignConsumable,
-//   restockConsumable,
-//   returnConsumable,
-//   deleteConsumable,
-// } from "../controllers/consumableController.js";
-// import { protect, isAdmin } from "../middleware/authMiddleware.js";
-
-// // --- AUTHENTICATION GUARD ---
-// router.use(protect);
-
-// router.get("/", getConsumables);
-// router.get("/:id", getConsumableById);
-
-// // --- ADMIN AUTHORIZATION GUARD ---
-// router.use((req, res, next) => isAdmin(req, res, next));
-
-// /**
-//  * Inventory Management
-//  */
-// router.post("/", createConsumable);
-// router.post("/:id/assign", assignConsumable);
-// router.post("/:id/return", returnConsumable);
-// router.patch("/:id/restock", restockConsumable);
-
-// // 2. ADD THE DELETE ROUTE HERE
-// router.delete("/:id", deleteConsumable);
-
-// export default router;
 import express from "express";
 const router = express.Router();
 import {
